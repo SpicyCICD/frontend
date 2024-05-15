7faa77fc0225fd2114365b1c3f082e9ddebc98ae
maheshshali25 <maiya9986@gmail.com>
2024-05-15 12:27:05 +0530

    first

---
+++ src/components/Admin.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const Admin = () => {
+    return (
+        <>
+            <Navbar />
+            <div>Admin</div>
+        </>
+
+    )
+}
+
+export default Admin
